c4eb6d5f9d7cfd5477209ec4abb2d9fa7ff10b18
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-08-06 22:38:47 +0300

    implemented error generation

--- common/error.model.go
+++ common/error.model.go
@@ -1,1 +1,18 @@
 package common
+
+type ApiError struct {
+	Message string         `json:"message"`
+	Status  int            `json:"status"`
+	Code    int            `json:"code,omitempty"`
+	Errors  []ErrorDetails `json:"errors,omitempty"`
+}
+
+type ErrorDetails struct {
+	Message string `json:"message"`
+	Code    int    `json:"code,omitempty"`
+	Field   string `json:"field,omitempty"`
+}
+
+func (e ApiError) Error() string {
+	return e.Message
+}
--- common/error.service.go
+++ common/error.service.go
@@ -1,1 +1,50 @@
 package common
+
+func newError(message string, status int) ApiError {
+	return ApiError{
+		Message: message,
+		Status:  status,
+	}
+}
+
+func NewCustomError(message string, status int, code int, errors ...ErrorDetails) ApiError {
+	return ApiError{
+		Message: message,
+		Status:  status,
+		Code:    code,
+		Errors:  errors,
+	}
+}
+
+func NewUnAuthorizedError(message string) ApiError {
+	return newError(message, 401)
+}
+
+func NewInternalServerError(message string) ApiError {
+	return newError(message, 500)
+}
+
+func NewNotFoundError(message string) ApiError {
+	return newError(message, 404)
+}
+
+func NewBadRequestError(message string) ApiError {
+	return newError(message, 400)
+}
+
+func NewValidationError(message string, errors ...ErrorDetails) ApiError {
+	return NewCustomError(message, 400, 0, errors...)
+}
+
+func GenerateErrorFromStatus(status int) ApiError {
+	switch status {
+	case 401:
+		return NewUnAuthorizedError("Unauthorized")
+	case 404:
+		return NewNotFoundError("Not Found")
+	case 400:
+		return NewBadRequestError("Bad Request")
+	default:
+		return NewInternalServerError("Internal Server Error")
+	}
+}
