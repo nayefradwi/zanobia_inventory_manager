8e0aa71b623b71eec66f8774c27c9d50060f40ec
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-08-06 22:43:57 +0300

    further modifications to errors

--- common/error.model.go
+++ common/error.model.go
@@ -1,5 +1,15 @@
 package common
 
+// only for common errors the codes are like their status codes
+// for custom errors the code will be based on the operation
+const (
+	NOT_FOUND             int = 404
+	BAD_REQUEST           int = 400
+	UNAUTHORIZED          int = 401
+	FORBIDDEN             int = 403
+	INTERNAL_SERVER_ERROR int = 500
+)
+
 type ApiError struct {
 	Message string         `json:"message"`
 	Status  int            `json:"status"`
--- common/error.service.go
+++ common/error.service.go
@@ -4,6 +4,7 @@ func newError(message string, status int) ApiError {
 	return ApiError{
 		Message: message,
 		Status:  status,
+		Code:    status,
 	}
 }
 
@@ -17,33 +18,39 @@ func NewCustomError(message string, status int, code int, errors ...ErrorDetails
 }
 
 func NewUnAuthorizedError(message string) ApiError {
-	return newError(message, 401)
+	return newError(message, UNAUTHORIZED)
 }
 
 func NewInternalServerError(message string) ApiError {
-	return newError(message, 500)
+	return newError(message, INTERNAL_SERVER_ERROR)
 }
 
 func NewNotFoundError(message string) ApiError {
-	return newError(message, 404)
+	return newError(message, NOT_FOUND)
 }
 
-func NewBadRequestError(message string) ApiError {
-	return newError(message, 400)
+func NewBadRequestError(message string, code int) ApiError {
+	return newError(message, BAD_REQUEST)
+}
+
+func NewForbiddenError(message string, code int) ApiError {
+	return newError(message, FORBIDDEN)
 }
 
 func NewValidationError(message string, errors ...ErrorDetails) ApiError {
-	return NewCustomError(message, 400, 0, errors...)
+	return NewCustomError(message, BAD_REQUEST, 0, errors...)
 }
 
 func GenerateErrorFromStatus(status int) ApiError {
 	switch status {
-	case 401:
+	case UNAUTHORIZED:
 		return NewUnAuthorizedError("Unauthorized")
-	case 404:
+	case NOT_FOUND:
 		return NewNotFoundError("Not Found")
-	case 400:
-		return NewBadRequestError("Bad Request")
+	case BAD_REQUEST:
+		return NewBadRequestError("Bad Request", status)
+	case FORBIDDEN:
+		return NewForbiddenError("Forbidden", status)
 	default:
 		return NewInternalServerError("Internal Server Error")
 	}
