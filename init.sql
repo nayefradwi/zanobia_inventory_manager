4602644f30edfbd5defc64ee11ce341bb0838bfa
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-08-11 21:25:10 +0300

    changed init sql

--- init.sql
+++ init.sql
@@ -1,10 +1,12 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    is_active BOOLEAN,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
     password VARCHAR(255)
 );
 
-CREATE INDEX idx_email ON users(email);
+DROP INDEX IF EXISTS idx_email;
+CREATE UNIQUE INDEX idx_email ON users(email);
