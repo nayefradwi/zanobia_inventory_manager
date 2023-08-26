9c4be9ab3871e42e4dd7b86e1bd7e09551c809f0
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-08-26 16:41:25 +0300

    updated init.sql and added unit model

--- init.sql
+++ init.sql
@@ -60,3 +60,39 @@ CREATE TABLE role_permissions (
 
 DROP INDEX IF EXISTS idx_role_permission;
 CREATE UNIQUE INDEX idx_role_permission ON role_permissions(role_id, permission_handle);
+
+CREATE TABLE units (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    symbol VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP INDEX IF EXISTS idx_name;
+CREATE UNIQUE INDEX idx_name ON units(name);
+
+DROP TABLE IF EXISTS unit_conversions;
+CREATE TABLE unit_conversions (
+    id SERIAL PRIMARY KEY,
+    unit_id INTEGER NOT NULL REFERENCES units(id),
+    conversion_unit_id INTEGER NOT NULL REFERENCES units(id),
+    conversion_factor NUMERIC(10, 2) NOT NULL
+);
+
+DROP INDEX IF EXISTS idx_unit_conversion;
+CREATE UNIQUE INDEX idx_unit_conversion ON unit_conversions(unit_id, conversion_unit_id);
+
+DROP TABLE IF EXISTS translations;
+CREATE TABLE translations (
+    id SERIAL PRIMARY KEY,
+    entity_id INTEGER NOT NULL,
+    translated_entity_id INTEGER NOT NULL,
+    entity_type VARCHAR(50) NOT NULL,
+    locale VARCHAR(2) NOT NULL DEFAULT 'ar',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP INDEX IF EXISTS idx_entity;
+CREATE UNIQUE INDEX idx_entity ON translations(entity_id, entity_type, locale);
---
+++ product/unit.model.go
@@ -0,0 +1,14 @@
+package product
+
+type Unit struct {
+	Id     *int   `json:"id,omitempty"`
+	Name   string `json:"name"`
+	Symbol string `json:"symbol"`
+}
+
+type UnitConversion struct {
+	Id               *int    `json:"id,omitempty"`
+	UnitId           *int    `json:"unitId"`
+	ConversionUnitId *int    `json:"conversionUnitId"`
+	ConversionFactor float32 `json:"conversionFactor"`
+}
