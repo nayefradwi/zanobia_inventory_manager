a80377386fd5f9d6e006269280ce71d0f99d3183
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-10-06 16:32:54 +0300

    basic update and create repo functions for batches

--- product/batch.repo.go
+++ product/batch.repo.go
@@ -1,6 +1,14 @@
 package product
 
-import "github.com/jackc/pgx/v4/pgxpool"
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/nayefradwi/zanobia_inventory_manager/common"
+	"github.com/nayefradwi/zanobia_inventory_manager/warehouse"
+)
 
 type IBatchRepository interface{}
 
@@ -12,7 +20,36 @@ func NewBatchRepository(pool *pgxpool.Pool) *BatchRepository {
 	return &BatchRepository{pool}
 }
 
-// Create batch
-// Edit batch quantity
-// Get batch base by product id and expiration date
-// Get batches paginated sorted by expiration date
+func (r *BatchRepository) CreateBatch(ctx context.Context, input BatchInput, expiresAt time.Time) error {
+	sql := `INSERT INTO batches (product_id, warehouse_id, quantity, unit_id, expires_at) VALUES ($1, $2, $3, $4, $5)`
+	op := common.GetOperator(ctx, r.Pool)
+	warehouseId := warehouse.GetWarehouseId(ctx)
+	_, err := op.Exec(ctx, sql, input.ProductId, warehouseId, input.Quantity, input.UnitId, expiresAt)
+	if err != nil {
+		log.Printf("Failed to create batch: %s", err.Error())
+		return common.NewBadRequestFromMessage("Failed to create batch")
+	}
+	return nil
+}
+
+func (r *BatchRepository) UpdateBatch(ctx context.Context, base BatchBase) error {
+	updatedAt := time.Now().UTC()
+	op := common.GetOperator(ctx, r.Pool)
+	sql := `UPDATE batches SET quantity = $1, updated_at = $2 WHERE id = $3`
+	_, err := op.Exec(ctx, sql, base.Quantity, updatedAt, base.Id)
+	if err != nil {
+		log.Printf("Failed to update batch: %s", err.Error())
+		return common.NewBadRequestFromMessage("Failed to update batch")
+	}
+	return nil
+}
+
+func (r *BatchRepository) GetBatchBase(ctx context.Context, productId int, expirationDate string) (Batch, error) {
+	// Get batch base by product id and expiration date
+	return Batch{}, nil
+}
+
+func (r *BatchRepository) GetBatches(ctx context.Context, pageSize int, cursor string) ([]Batch, error) {
+	// Get batches paginated sorted by expiration date
+	return []Batch{}, nil
+}
