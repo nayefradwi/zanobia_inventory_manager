42a3d59f1fbbdce35c8fba12fcb63af9f9a3f3de
Nayef Radwi <45208221+nayefradwi@users.noreply.github.com>
2023-12-09 20:54:50 +0300

    implemented production logger

--- common/logger.service.go
+++ common/logger.service.go
@@ -2,13 +2,37 @@ package common
 
 import (
 	"context"
+	"os"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var logger *zap.Logger
+var rollingLogger *lumberjack.Logger = &lumberjack.Logger{
+	Filename:   "/var/logs/server.log",
+	MaxSize:    100,
+	MaxBackups: 3,
+	MaxAge:     30,
+}
 
 type loggerContextKey struct{}
+
+func SetMaxSize(maxSize int) *lumberjack.Logger {
+	rollingLogger.MaxSize = maxSize
+	return rollingLogger
+}
+
+func SetMaxBackups(maxBackups int) *lumberjack.Logger {
+	rollingLogger.MaxBackups = maxBackups
+	return rollingLogger
+}
+
+func SetMaxAge(maxAge int) *lumberjack.Logger {
+	rollingLogger.MaxAge = maxAge
+	return rollingLogger
+}
 
 func InitializeLogger() {
 	if IsDev() {
@@ -20,16 +44,22 @@ func InitializeLogger() {
 		logger.Info("logger initialized")
 	}
 }
-
-func initializeProdLogger() {
-	// TODO: implement
-	// this needs to out put to file and stdout
-	// and also needs to be rotated
-	// and also needs to be sent to a log aggregator (maybe)
-}
-
 func initializeDevLogger() {
 	logger = zap.Must(zap.NewDevelopment())
+}
+
+func initializeProdLogger() {
+	pe := zap.NewProductionEncoderConfig()
+	pe.EncodeTime = zapcore.ISO8601TimeEncoder
+	fileEncoder := zapcore.NewJSONEncoder(pe)
+	consoleEncoder := zapcore.NewConsoleEncoder(pe)
+	level := zap.InfoLevel
+	rollingLogsSync := zapcore.AddSync(rollingLogger)
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, rollingLogsSync, level),
+		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), level),
+	)
+	logger = zap.New(core)
 }
 
 func GetLogger() *zap.Logger {
--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
